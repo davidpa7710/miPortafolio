c94053a03decbc8fd854f9152cae38c6ebe8642b
davidpa7710 <davidpa7710@gmail.com>
2021-11-23 21:17:02 -0600

    Operacion Multiplicacion sin signo *

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,13 @@
 console.log('hola mundo')
+
+//Multiplicar dos numeros sin usar el simbolo de multiplicar 
+
+function mult(a,b) {
+    let resultado = 0;
+    for (let index = 1; index <= b; index++) {
+        resultado = resultado + a;
+    }
+    return resultado
+}
+console.log(mult(6,2));
+
